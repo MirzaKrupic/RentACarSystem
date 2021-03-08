0826300550686d6147ee7a2a30743818421a993e
Mirza <mirza.krupic@stu.ibu.edu.ba>
2021-03-08 11:54:14 +0100

    BaseDao

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # RentACarSystem
+
+This is Rent a car web application intended for users/companies to post cars for renting and users to rent them.
---
+++ api/config.php
@@ -0,0 +1,10 @@
+<?php
+
+class Config {
+  const DB_HOST = "localhost";
+  const DB_USERNAME = "rentacarsystem";
+  const DB_PASSWORD = "rentacarsystem";
+  const DB_SCHEME = "rentacarsystem";
+}
+
+?>
