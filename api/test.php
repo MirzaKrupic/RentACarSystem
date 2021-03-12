99d41c79e45371b5a953cce87be4d6e62dd43ff1
Mirza <mirza.krupic@stu.ibu.edu.ba>
2021-03-12 12:25:01 +0100

    CarDao

--- api/test.php
+++ api/test.php
@@ -1,15 +1,18 @@
 <?php
   require_once dirname(__FILE__)."/dao/UserDao.class.php";
   require_once dirname(__FILE__)."/dao/CompanyDao.class.php";
-    require_once dirname(__FILE__)."/dao/BrandDao.class.php";
+  require_once dirname(__FILE__)."/dao/BrandDao.class.php";
+  require_once dirname(__FILE__)."/dao/CarDao.class.php";
 
-  $user_dao = new BrandDao();
+  $user_dao = new CarDao();
 
   $user1 = [
-    "name"=> "Audi"
+    "model" => "A6",
+    "brand_id" => "1",
+    "owner_id" => "1"
   ];
 
-  $user = $user_dao->get_all_brands();
+  $user = $user_dao->get_car_by_owner_id(1);
 
   print_r($user);
 
