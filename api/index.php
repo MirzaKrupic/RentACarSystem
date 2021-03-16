c93c23d45974d85994683923ed55945c6cef128f
Mirza <mirza.krupic@stu.ibu.edu.ba>
2021-03-16 21:29:08 +0100

    Basic routes

--- api/index.php
+++ api/index.php
@@ -2,13 +2,27 @@
 require_once dirname(__FILE__)."/dao/BaseDao.class.php";
 
 require dirname(__FILE__).'/../vendor/autoload.php';
+require dirname(__FILE__).'/dao/UserDao.class.php';
 
-Flight::route('/', function(){
-    echo 'hello world!';
+Flight::register('userdao', 'UserDao');
+
+Flight::route('GET /users', function(){
+  Flight::json(Flight::userdao()->get_all(0,10));
 });
 
-Flight::route('/hello2', function(){
-    echo 'hello world!2';
+Flight::route('GET /users/@id', function($id){
+  Flight::json(Flight::userdao()->get_by_id($id));
+});
+
+Flight::route('POST /users', function(){
+  $data = Flight::request()->data->getData();
+  Flight::json(Flight::userdao()->add($data));
+});
+
+Flight::route('PUT /users/@id', function($id){
+  $data = Flight::request()->data->getData();
+  Flight::userdao()->update($id, $data);
+  print_r(Flight::userdao()->get_by_id($id));
 });
 
 Flight::start();
