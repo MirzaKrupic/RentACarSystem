1088accb03ae11b8f1e2a7a262397cc908a3b53a
Mirza <mirza.krupic@stu.ibu.edu.ba>
2021-03-17 17:31:19 +0100

    Search users

--- api/dao/UserDao.class.php
+++ api/dao/UserDao.class.php
@@ -5,5 +5,11 @@ class UserDao extends BaseDao{
   public function __construct(){
     parent::__construct("users");
   }
+
+  public function get_users($search, $offset, $limit){
+    return $this->query("SELECT * FROM users
+                        WHERE LOWER(name) LIKE CONCAT('%', :name, '%')
+                        LIMIT ${limit} OFFSET ${offset}", ["name" => strtolower($search)]);
+  }
 }
 ?>
