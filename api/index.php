a09b5d12d222a20db652406445efeceeb1b0fd5e
Mirza <mirza.krupic@stu.ibu.edu.ba>
2021-03-17 17:08:23 +0100

    parsing of query parameters

--- api/index.php
+++ api/index.php
@@ -4,6 +4,14 @@ require_once dirname(__FILE__)."/dao/BaseDao.class.php";
 require_once dirname(__FILE__).'/../vendor/autoload.php';
 require_once dirname(__FILE__).'/dao/UserDao.class.php';
 
+Flight::map('query', function($name, $default_value = NULL){
+    $request = Flight::request();
+
+    $query_param = @$request->query->getData()[$name];
+    $query_param = $query_param ? $query_param : $default_value;
+    return $query_param;
+});
+
 Flight::register('userdao', 'UserDao');
 
 require_once dirname(__FILE__).'/routes/users.php';
