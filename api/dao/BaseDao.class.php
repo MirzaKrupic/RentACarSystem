fb8bf601b80ad16a5ac7f87aa7fa91582d0af245
Mirza <mirza.krupic@stu.ibu.edu.ba>
2021-06-21 12:09:17 +0200

    change config file to read env variables

--- api/dao/BaseDao.class.php
+++ api/dao/BaseDao.class.php
@@ -23,7 +23,7 @@ class BaseDao {
   public function __construct($table){
     $this->table = $table;
     try {
-      $this->connection = new PDO("mysql:host=".Config::DB_HOST.";dbname=".Config::DB_SCHEME, Config::DB_USERNAME, Config::DB_PASSWORD);
+      $this->connection = new PDO("mysql:host=".Config::DB_HOST().";dbname=".Config::DB_SCHEME(), Config::DB_USERNAME(), Config::DB_PASSWORD());
       $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch(PDOException $e) {
       throw $e;
