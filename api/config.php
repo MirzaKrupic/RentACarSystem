31467d438d918bd8d554bdc5a4cb3279de1a3947
Mirza <mirza.krupic@stu.ibu.edu.ba>
2021-06-21 11:37:12 +0200

    add config functions

--- api/config.php
+++ api/config.php
@@ -3,15 +3,33 @@
 class Config {
   const DATE_FORMAT = "Y-m-d H:i:s";
 
-  const DB_HOST = "localhost";
-  const DB_USERNAME = "rentacarsystem";
-  const DB_PASSWORD = "rentacarsystem";
-  const DB_SCHEME = "rentacarsystem";
-
-  const SMTP_HOST ="smtp.googlemail.com";
-  const SMTP_PORT = 465;
-  const SMTP_USER ="mirza.krupic@stu.ibu.edu.ba";
-  const SMTP_PASSWORD ="";
+  public static function DB_HOST(){
+  return Config::get_env("DB_HOST", "localhost");
+  }
+  public static function DB_USERNAME(){
+    return Config::get_env("DB_USERNAME", "rentacarsystem");
+  }
+  public static function DB_PASSWORD(){
+    return Config::get_env("DB_PASSWORD", "rentacarsystem");
+  }
+  public static function DB_SCHEME(){
+    return Config::get_env("DB_SCHEME", "rentacarsystem");
+  }
+  public static function DB_PORT(){
+    return Config::get_env("DB_PORT", "3306");
+  }
+  public static function SMTP_HOST(){
+    return Config::get_env("SMTP_HOST", "smtp.googlemail.com");
+  }
+  public static function SMTP_PORT(){
+    return Config::get_env("SMTP_PORT", "465");
+  }
+  public static function SMTP_USER(){
+    return Config::get_env("SMTP_USER", "mirza.krupic@stu.ibu.edu.ba");
+  }
+  public static function SMTP_PASSWORD(){
+    return Config::get_env("SMTP_PASSWORD", NULL);
+  }
 
   const JWT_SECRET = "y4KvQcZVqn3F7uxQvcFk";
   const JWT_TOKEN_TIME = 604800;
