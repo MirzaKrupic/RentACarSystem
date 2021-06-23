eb4eacaee1a0613579a0338d6061f8db975f655a
Mirza <mirza.krupic@stu.ibu.edu.ba>
2021-06-23 23:28:46 +0200

    add company update cdn

--- api/routes/cdn.php
+++ api/routes/cdn.php
@@ -36,3 +36,22 @@ Flight::route('POST /user/cdn', function(){
   $url = Flight::cdnClient()->upload($data['name'], $data['content']);
   Flight::json(["url" => $url]);
 });
+
+/**
+ * @OA\Post(path="/companies/cdn", tags={"companies", "cdn"}, security={{"ApiKeyAuth": {}}},
+ *   @OA\RequestBody(description="Upload file to CDN", required=true,
+ *       @OA\MediaType(mediaType="application/json",
+ *    			@OA\Schema(
+ *    				 @OA\Property(property="name", required="true", type="string", example="name",	description="FileName" ),
+ *    				 @OA\Property(property="content", required="true", type="string", example="test",	description="Base64 encoded content" )
+ *          )
+ *       )
+ *     ),
+ *  @OA\Response(response="200", description="Upload file to CDN")
+ * )
+ */
+Flight::route('POST /companies/cdn', function(){
+  $data = Flight::request()->data->getData();
+  $url = Flight::cdnClient()->upload($data['name'], $data['content']);
+  Flight::json(["url" => $url]);
+});
